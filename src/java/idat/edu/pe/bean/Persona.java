06968500b64fb6a4e380853c8afa48a9150e41f9
LuisAngel <LuisAngel@172.27.1.32>
2018-10-05 17:58:54 -0500

    Primer commit.

--- src/java/idat/edu/pe/bean/Persona.java
+++ src/java/idat/edu/pe/bean/Persona.java
@@ -6,6 +6,7 @@
 package idat.edu.pe.bean;
 
 import java.io.Serializable;
+import javax.validation.constraints.Pattern;
 import javax.xml.bind.annotation.XmlRootElement;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
@@ -16,11 +17,24 @@ import org.hibernate.validator.constraints.NotEmpty;
  */
 @XmlRootElement(name = "persona")
 public class Persona implements Serializable {
-    private static final long serialVerisionUID = 1L;
+    //private static final long serialVerisionUID = 1L;
     
     @NotEmpty
     @Length(min = 3, max = 10)
     private String nombre;
+    
+    @NotEmpty
+    @Pattern(regexp = "((?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%&*]).{6,20})")
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
 
     public String getNombre() {
         return nombre;
